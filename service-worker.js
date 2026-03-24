be05157a49c5ce82e2c90f64249bc2ccd9232786
kakashikempachi <kakashikempachi@gmail.com>
2026-03-23 23:08:43 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,6 @@
-const CACHE = "prova-cache-v1";
-
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
+    caches.open("cache-v1").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -15,8 +13,6 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
