fa3df48d2ac4aa78385e2a93f966b3a92332c5eb
kakashikempachi <kakashikempachi@gmail.com>
2026-09-04 11:55:31 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,30 @@
-self.addEventListener("install", e => {
+const CACHE = 'gerador-provas-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open("cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./styles.css",
-        "./app.js"
-      ]);
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return cached || fetch(e.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match('./index.html'));
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('message', (e) => {
+  if (e.data === 'SKIP_WAITING') self.skipWaiting();
 });
